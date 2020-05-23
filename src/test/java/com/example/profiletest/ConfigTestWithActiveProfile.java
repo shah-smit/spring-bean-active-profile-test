de8f9a6b82acf4f7e1b741fcf3a2ec38a3c9ab3b
Smit <smitshah95@hotmail.com>
2020-05-23 18:23:31 +0800

    added properties tests

--- src/test/java/com/example/profiletest/ConfigTestWithActiveProfile.java
+++ src/test/java/com/example/profiletest/ConfigTestWithActiveProfile.java
@@ -5,14 +5,19 @@ import com.example.profiletest.dummy_files.QuickSortAlgorithm;
 import com.example.profiletest.dummy_files.SortAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("sg")
 public class ConfigTestWithActiveProfile {
+
+    @Value("${service.title}")
+    private String title;
 
     @Autowired
     private SortAlgorithm sortAlgorithm;
@@ -20,5 +25,10 @@ public class ConfigTestWithActiveProfile {
     @Test
     public void WithActiveProfileSG_ItShouldBindQuickSortAlgorithm(){
         assertThat(sortAlgorithm).isExactlyInstanceOf(QuickSortAlgorithm.class);
+    }
+
+    @Test
+    public void ActiveProfileSG_ItShouldPickSGRelated(){
+        assertEquals("singapore", title);
     }
 }
--- src/test/java/com/example/profiletest/ProfiletestApplicationTests.java
+++ src/test/java/com/example/profiletest/ProfiletestApplicationTests.java
@@ -4,9 +4,11 @@ import com.example.profiletest.dummy_files.BubbleSortAlgorithm;
 import com.example.profiletest.dummy_files.SortAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ProfiletestApplicationTests {
@@ -15,6 +17,9 @@ class ProfiletestApplicationTests {
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
 
+	@Value("${service.title}")
+	private String title;
+
 	@Test
 	void contextLoads() {
 	}
@@ -22,5 +27,10 @@ class ProfiletestApplicationTests {
 	@Test
 	public void WithDefaultProfile_ItShouldBindBubbleSortAlgorithm() {
 		assertThat(sortAlgorithm).isExactlyInstanceOf(BubbleSortAlgorithm.class);
+	}
+
+	@Test
+	public void WithDefaultProfile_ItShouldPickSGRelated(){
+		assertEquals("maintitle", title);
 	}
 }
